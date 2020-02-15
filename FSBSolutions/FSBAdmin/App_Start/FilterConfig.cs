54b5e37cb767e7bd87234839fa2adc8f6d559bf8
tpabhilash10 <tpabhilash10@gmail.com>
2020-02-15 19:10:43 +0530

    admin changes with code template

--- FSBSolutions/FSBAdmin/App_Start/FilterConfig.cs
+++ FSBSolutions/FSBAdmin/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace FSBAdmin
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            
         }
     }
 }
