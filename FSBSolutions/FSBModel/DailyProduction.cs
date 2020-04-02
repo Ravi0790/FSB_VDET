617bf21ec1b69214c551f530cc343af1b8b5cc60
ravi srivastava <srivastava.ashish08@gmail.com>
2020-04-02 20:46:20 +0530

    DailyProduction table created

---
+++ FSBSolutions/FSBModel/DailyProduction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSBModel
+{
+    class DailyProduction
+    {
+        public int Id { get; set; }
+        public DateTime CDate { get; set; }
+        public string Line { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDesc { get; set; }
+        public string ProductCountry { get; set; }
+        
+    }
+}
