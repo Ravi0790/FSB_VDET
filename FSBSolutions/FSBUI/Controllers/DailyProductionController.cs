32dc1d5ddb69aaa939d831e1b1f14b044b20e335
ravi srivastava <srivastava.ashish08@gmail.com>
2020-04-03 23:12:09 +0530

    Added new model view n controller DailyProduction

---
+++ FSBSolutions/FSBUI/Controllers/DailyProductionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using FSBModel;
+
+namespace FSBUI.Controllers
+{
+    public class DailyProductionController : Controller
+    {
+        // GET: DailyProduction
+
+        // GET: Line Name
+        private FSBDBContext db = new FSBDBContext();
+        public ActionResult Index()
+        {
+            ViewBag.Line = new SelectList(db.Lines, "LineId", "LineName");
+
+            return View();
+        }
+        
+    }
+}
